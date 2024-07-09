15b9260c159094361290a9e7231cb18dbdc93503
Nicole-125 <ns.vizhnay.2016@gmail.com>
2024-07-09 21:50:13 +0200

    primer commit

---
+++ src/funciones.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener el elemento del botón por su ID
+    var boton = document.getElementById('botoncito3');
+
+    // Verificar que el elemento existe
+    if (boton) {
+        // Agregar un listener para el evento 'click'
+        boton.addEventListener('click', function() {
+            alert('¡Botón clicado!');
+            // Aquí puedes agregar más funcionalidad que desees
+        });
+    } else {
+        console.error('No se encontró el botón con ID "botoncito3".');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener el elemento del botón por su ID
+    var boton = document.getElementById('loginButton');
+
+    // Verificar que el elemento existe
+    if (boton) {
+        // Agregar un listener para el evento 'click'
+        boton.addEventListener('click', function() {
+            //alert('REDIRECCIONA AL LOGGING');
+            // Aquí puedes agregar más funcionalidad que desees
+            // Redireccionar a loggin.html
+            window.location.href = './loggin.html';
+        });
+    } else {
+        console.error('No se encontró el botón con ID "loginButton".');
+    }
+});
